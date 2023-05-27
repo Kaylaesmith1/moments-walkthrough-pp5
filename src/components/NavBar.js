5a5fd1e9606ccdb21549dadfef95bf4e730361bb
Kayla S <kaylaesmith1@gmail.com>
2023-05-27 19:00:16 +0000

    Configuration for posts and cancel.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,6 @@
+
+// PROBLEM W AVATAR IMAGE (LOOK IN OTHER GITPOD THAT'S CONNECTED). ASK ABOUT NAME CHANGE OF IMAGE ON SLACK?
+
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
@@ -9,10 +12,15 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
 
   const handleSignOut = async () => {
     try {
@@ -80,7 +88,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -88,7 +96,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+        ref={ref}
+        onClick={() => setExpanded(!expanded)}
+        aria-controls="basic-navbar-nav" 
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
