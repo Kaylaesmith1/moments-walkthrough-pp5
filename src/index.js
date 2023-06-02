ee07c818fc604b8df1c4ddf668f193ffba70fc1c
Kayla S <kaylaesmith1@gmail.com>
2023-06-02 09:16:03 +0000

    Enable user to follow another user

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
 
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <Router>
 //     <CurrentUserProvider>
+//     <ProfileDataProvider>
 //         <App />
+//         </ProfileDataProvider>
 //       </CurrentUserProvider>
 //     </Router>
 //   </React.StrictMode>,
@@ -21,7 +24,9 @@ import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 ReactDOM.render(
   <Router>
     <CurrentUserProvider>
+    <ProfileDataProvider>
     <App />
+    </ProfileDataProvider>
     </CurrentUserProvider>
   </Router>,
 document.getElementById('root')
